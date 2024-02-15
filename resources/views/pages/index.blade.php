36e79af93a92e48d54112315bfaa4971adfc2728
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-15 18:58:59 +0100

    Minority modify

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,13 +4,15 @@
 @endsection
 @section('content')
     <h1>Comics: {{count($comics)}}</h1>
+    <button class="btn btn-primary"><a href="{{ route('comics.create') }}">Aggiungi Comic</a></button>
     <ul>
         @foreach ($comics as $comic)
-            <li>Title: {{$comic -> title}} <br>
+            <li><a href="{{ route('comics.show', $comic -> id) }}">Title: {{$comic -> title}} <br>
                 Author: {{$comic -> author}} <br>
                 Genre: {{$comic -> genre}} <br>
                 Price: €{{$comic -> price}}
+                </a>
             </li> 
         @endforeach
-    </ul>
+    </ul>    
 @endsection
