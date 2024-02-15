3cd5a7a8bff8c1ed2f562c5db02538820d3c5852
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-15 18:59:44 +0100

    Create new route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComicController;
 
-Route::get('/', [ComicController :: class, 'index']);
+Route::get('/', [ComicController :: class, 'index']) -> name('comics.index');
+Route::get('/comics/create', [ComicController :: class, 'create']) -> name('comics.create');
+Route::post('/comics', [ComicController :: class, 'store']) -> name('comics.store');
+Route::get('/comics/{id}', [ComicController :: class, 'show']) -> name('comics.show');
