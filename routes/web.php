13e5fb5deebb1d2fa364e5c91d889903ff079083
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-16 16:50:57 +0100

    Nuove rotte per destroy, update e edit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComicController;
 
 Route::get('/', [ComicController :: class, 'index']) -> name('comics.index');
+
 Route::get('/comics/create', [ComicController :: class, 'create']) -> name('comics.create');
+
 Route::post('/comics', [ComicController :: class, 'store']) -> name('comics.store');
+
 Route::get('/comics/{id}', [ComicController :: class, 'show']) -> name('comics.show');
+
+Route::delete('/comics/{id}', [ComicController :: class,  'destroy']) ->name('comics.destroy');
+
+Route::get('/comics/{id}/edit', [ComicController :: class, 'edit']) ->name('comics.edit');
+
+Route::put('/comics/{id}', [ComicController :: class, 'update']) ->name('comics.update');
