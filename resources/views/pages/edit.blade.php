7e0610e5b115757b836baca564b6979f520766d4
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-19 12:28:48 +0100

    Aggiunta stampa di errore con validazione/ Consegna

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -13,38 +13,92 @@
                 <div class="col ">
                     <label for="title">Title</label>
                     <input type="text" name="title" value="{{$comics -> title}}">
+                    <br>
+                    @error('title')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="editor">Editor</label>
                     <input type="text" name="editor" value="{{$comics -> editor}}">
+                    <br>
+                    @error('editor')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="author">Author</label>
                     <input type="text" name="author" value="{{$comics -> author}}">
+                    <br>
+                    @error('author')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="designer">Designer</label>
                     <input type="text" name="designer" value="{{$comics -> designer}}">
+                    <br>
+                    @error('designer')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="genre">Genre</label>
                     <input type="text" name="genre" value="{{$comics -> genre}}">
+                    <br>
+                    @error('genre')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="price">Price</label>
                     <input type="text" name="price" value="{{$comics -> price}}">
+                    <br>
+                    @error('price')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="pubblication_year">Pubblication Year</label>
                     <input type="text" name="pubblication_year" value="{{$comics -> pubblication_year}}">
+                    <br>
+                    @error('pubblication_year')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="quantity_in_store">Quantity in store</label>
                     <input type="text" name="quantity_in_store" value="{{$comics -> quantity_in_store}}">
+                    <br>
+                    @error('quantity_in_store')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col">
                     <label for="description">Description</label>
                     <input type="text" name="description" value="{{$comics -> description}}">
+                    <br>
+                    @error('description')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <input class="m-5" type="submit" value="Update">
